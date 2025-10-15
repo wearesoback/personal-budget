aba6cc16929218b133a034fdb978bc49e290a3a1
Christian Lopes <backweare@protonmail.com>
2025-10-15 21:24:16 +0100

    created first GET / endpoint

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res, next) =>{
+    res.send('Personal Budget Project...')
+})
 
 // export app for use in main.js and for testing
 module.exports = {
